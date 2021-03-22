9cd32a1ac01f2fbf8b67937fc554f501b293f047
jcaipap <jcaipap@unal.edu.co>
2021-03-21 21:30:44 -0500

    jcaipap

--- hdl/src/sum1bcc_primitive.v
+++ hdl/src/sum1bcc_primitive.v
@@ -1,15 +1,19 @@
-module sum1bcc_primitive (A, B, Ci,Cout,S);
+module sum1bcc_primitive (A, B, Ci,Cout,S); //Se define el módulo con las 3 entradas y las 2 sálidas 
 
+	//Se definen las entradas y salidas
   input  A;
   input  B;
-  input  Ci;
+  input  Ci; // Entradas
   output Cout;
-  output S;
+  output S; //Sálidas
 
+  //Se definen los cables que conectarán el sumador
 
-  wire a_ab;
+  wire a_ab; 
   wire x_ab;
   wire cout_t;
+  
+  //Se definen los operadores lógicos
 
   and(a_ab,A,B);
   xor(x_ab,A,B);
@@ -18,5 +22,4 @@ module sum1bcc_primitive (A, B, Ci,Cout,S);
   and(cout_t,x_ab,Ci);
 
   or (Cout,cout_t,a_ab);
-endmodule
-
+endmodule //Se finaliza el módulo
